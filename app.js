b37362b13e98dd3e123bdf53258b38ad7d7eb28b
Ashish7520 <ashishnandwana2@gmail.com>
2023-03-24 19:53:24 +0530

    login frontend page

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const http = require('http')
 const express = require('express')
 const app = express();
-app.use((req,res,next)=>{
-    console.log('into the middleware')
-})
+const User = require('./model/User');
+const sequelize = require('./util/database');
+const bodyparser = require('body-parser')
+app.use(bodyparser.json());
+const cors = require('cors')
+app.use(cors());
 
-app.listen(3500);
+
+app.post('/user/signup',(req,res,next)=>{
+    try {
+        const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    //console.log('username',username,'email',email,'password',password)
+    User.create({username:username,email:email,password:password}).then(()=>{
+        res.status(201).json({massage : 'user created successfully'})
+    }).catch(err=>{
+        res.status(500).json(err)
+    })
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+sequelize.sync().then(result=>{
+    app.listen(3500);
+}).catch(err=>{
+    console.log(err)
+})
