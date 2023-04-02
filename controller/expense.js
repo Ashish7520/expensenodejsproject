f7665eed404d6b40dddc457f8cc7170313c0be48
Ashish7520 <ashishnandwana2@gmail.com>
2023-04-02 16:51:56 +0530

    making app production ready

--- controller/expense.js
+++ controller/expense.js
@@ -1,8 +1,10 @@
 const Expense = require('../model/Expense');
 const User = require('../model/User');
+const sequelize = require('../util/database')
 
 
 const postExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();  
     try {
         const expense = req.body.expense;
         const descreption = req.body.descreption;
@@ -13,14 +15,19 @@ const postExpense = async (req, res, next) => {
             descreption: descreption,
             catagory: catagory,
             userId:req.user.id
+        },{
+            transaction:t
         });
         const totalExpense = Number(req.user.totalExpenses)+Number(expense)
         console.log(totalExpense)
-        User.update({totalExpenses:totalExpense},{
-            where:{id:req.user.id}
+       await User.update({totalExpenses:totalExpense},{
+            where:{id:req.user.id},
+            transaction : t
         })
+        await t.commit()
         res.status(200).json({expenseDetail:expenseDetail})
     } catch (err) {
+        await t.rollback();
         res.status(404).json({err:err})
     }
 };
@@ -35,20 +42,34 @@ const getExpense = async (req,res,next) => {
     }
 };
 
-const deleteExpense = async (req,res,next) => {
+const deleteExpense = async (req, res, next) => {
     console.log(req.params);
+    const t = await sequelize.transaction();
+    const id = req.params.id;
+    const expenseOfSubs = await Expense.findOne({ where: { id: id, userId: req.user.id }});
+        console.log("expense>>>>>>>>>",expenseOfSubs.expense)
     try {
         const id = req.params.id;
-        const product = await Expense.destroy({ where: { id: id,userId:req.user.id} }).then(noofrow=>{
-            if(noofrow===0){
-                return res.status(404).json({success:false, massage:'expense belongs to other user'})
-            }
-            res.json(product)
-        });
+        const noOfRowsDeleted = await Expense.destroy({ where: { id: id, userId: req.user.id },transaction:t });
+        if (noOfRowsDeleted === 0) {
+            return res.status(404).json({ success: false, message: 'Expense belongs to other user' });
+        }
+        
+        const totalExpense = Number(req.user.totalExpenses) - Number(expenseOfSubs.expense);
+
+        console.log("totalExpense>>>>>>>>>>>>>>>",totalExpense)
+        await User.update({totalExpenses:totalExpense},{
+            where:{id:req.user.id},
+            transaction:t
+        })
+        await t.commit();
+        res.json({ success: true, message: 'Expense deleted successfully' });
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: err.message });
+        await t.rollback();
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong' });
     }
 };
+
 
 module.exports = { postExpense, getExpense, deleteExpense };
